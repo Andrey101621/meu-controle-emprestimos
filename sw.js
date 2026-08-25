4a1106b6c1efba1cf71402d877204ad1b82b49d2
Andrey101621 <165973482+Andrey101621@users.noreply.github.com>
2026-08-25 17:45:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,81 +1,85 @@
-const CACHE_NAME = "meu-controle-emprestimos-v6";
+const CACHE_NAME = "meu-controle-emprestimos-v8";
 
 const ARQUIVOS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./sw.js"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ARQUIVOS))
       .then(() => self.skipWaiting())
-
   );
-
 });
 
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys()
       .then(cachesExistentes => {
-
         return Promise.all(
-
           cachesExistentes
             .filter(cache => cache !== CACHE_NAME)
             .map(cache => caches.delete(cache))
-
         );
-
       })
       .then(() => self.clients.claim())
-
   );
-
 });
-
 
 self.addEventListener("fetch", event => {
 
-  if(event.request.method !== "GET"){
+  if (event.request.method !== "GET") {
     return;
   }
 
   event.respondWith(
-
     fetch(event.request)
       .then(resposta => {
 
-        const copia =
-          resposta.clone();
+        const copia = resposta.clone();
 
         caches.open(CACHE_NAME)
           .then(cache => {
-
-            cache.put(
-              event.request,
-              copia
-            );
-
+            cache.put(event.request, copia);
           });
 
         return resposta;
-
       })
       .catch(() => {
-
-        return caches.match(
-          event.request
-        );
-
+        return caches.match(event.request);
       })
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+
+  event.notification.close();
+
+  event.waitUntil(
+
+    clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    })
+
+    .then(lista => {
+
+      for (const cliente of lista) {
+
+        if ("focus" in cliente) {
+          return cliente.focus();
+        }
+
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow("./");
+      }
+
+    })
 
   );
 
